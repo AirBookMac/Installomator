8298e39de946eeb69a81e08ea5f18d8fbf791654
Mirko Steinbrecher <98949095+AirBookMac@users.noreply.github.com>
2025-11-17 14:31:14 +0100

    Update Installomator.sh
    Edit Sketch

--- Installomator.sh
+++ Installomator.sh
@@ -8885,8 +8885,8 @@ sizeup)
 sketch)
     name="Sketch"
     type="zip"
-    downloadURL=$(curl -sf https://www.sketch.com/downloads/mac/ | grep 'href="https://download.sketch.com' | tr '"' "\n" | grep -E "https.*.zip")
-    appNewVersion=$( grep -oE '\d+\.\d+' <<< $downloadURL)
+    downloadURL=$(curl -sf https://www.sketch.com/downloads/mac/ | grep -m 1 'href="https://download.sketch.com' | tr '"' "\n" | grep -E "https.*.zip")
+    appNewVersion=$(echo "$downloadURL" | awk -F'-' '{ print $2 }')
     expectedTeamID="WUGMZZ5K46"
     ;;
 sketchupviewer)
